be9125168beab947d0b6ad4f4e2e225f120481fb
danimsteger <daniellestegertutoring@gmail.com>
2024-07-23 19:21:25 -0400

    login and signup form js and pages

--- public/js/login.js
+++ public/js/login.js
@@ -12,7 +12,7 @@ const loginForm = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/homepage');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
@@ -21,6 +21,26 @@ const loginForm = async (event) => {
 
 const signupForm = async (event) => {
   event.preventDefault();
+
+  const name = document.querySelector('#name-signup').value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
 };
 
 document.querySelector('.login-form').addEventListener('submit', loginForm);
+
+document.querySelector('.signup-form').addEventListener('submit', signupForm);
